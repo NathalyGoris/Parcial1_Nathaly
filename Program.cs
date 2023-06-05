695eb083654d493fbd33582ed6ec15e5b2acd639
Nathaly Goris Espino <nathalygorisespino@outlook.com>
2023-06-05 19:01:51 -0400

    InyectandoElConnectionString

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 //using Parcial1.Data;
 
 var builder = WebApplication.CreateBuilder(args);
