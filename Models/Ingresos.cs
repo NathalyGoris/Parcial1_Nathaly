2e4ee42ac281611d8afd80e15a2b802b31369946
Nathaly Goris Espino <nathalygorisespino@outlook.com>
2023-06-07 16:07:54 -0400

    Agregando ErrorMessage al Modelo

--- Models/Ingresos.cs
+++ Models/Ingresos.cs
@@ -6,7 +6,13 @@ public class Ingresos
 
     [Key]
     public int IngresoId { get; set; }
+
+    [Required(ErrorMessage ="La fecha del ingreso es obligatoria")]
     public string? Fecha { get; set; }
+
+    [Required(ErrorMessage ="El concepto del ingreso es obligatorio")]
     public string? Concepto { get; set; }
+
+    [Required(ErrorMessage ="El monto del ingreso es obligatorio")]
     public string? Monto { get; set; }    
 }
